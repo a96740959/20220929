2c2f2c282a35f124ca5a4d8a9aa292d367ae1e9b
a96740959 <113577955+a96740959@users.noreply.github.com>
2022-11-03 19:26:51 +0800

    20221103

---
+++ Diu/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Alex
+{
+    /// <summary>
+    /// First Script
+    /// </summary>
+    public class FirstScript : MonoBehaviour
+    {
+        #region lol
+        //???
+        /* dwa
+         * 
+         * dwa
+         * 
+         */
+        #endregion
+        #region Event
+
+        #endregion
+    }
+}
